e6c8d19ff7aa51c87af4e5c89a3fc4b0ab5452ab
Erik Fried <erik.fried@aftonbladet.se>
2017-02-26 00:36:19 +0100

    start

--- routes.js
+++ routes.js
@@ -4,6 +4,10 @@ const router = new Router();
 router
     .get('/', ctx => {
         ctx.body = 'Hello Koa';
+    })
+    .post('/webhook', (ctx, next) => {
+        const data = JSON.parse(ctx.body);
+        
     });
 
 module.exports = router.routes();
