66f45f9aa3c7752be753b125f418c463f129b123
Erik Fried <erik.fried@aftonbladet.se>
2017-02-26 11:32:11 +0100

    Set status

--- webhook/index.js
+++ webhook/index.js
@@ -1,25 +1,26 @@
 const HEADERS = {
-    event: 'X-GitHub-Event',        // Name of the event that triggered this delivery.
-    signature: 'X-Hub-Signature',   // HMAC hex digest of the payload, using the hook's secret as the key (if configured).
-    delivery: 'X-GitHub-Delivery'   // Unique ID for this delivery.
+    event: 'X-GitHub-Event'.toLowerCase(),        // Name of the event that triggered this delivery.
+    signature: 'X-Hub-Signature'.toLowerCase(),   // HMAC hex digest of the payload, using the hook's secret as the key (if configured).
+    delivery: 'X-GitHub-Delivery'.toLowerCase()   // Unique ID for this delivery.
 };
 
 async function issuesHandler(ctx, next) {
     console.log('ctx', ctx.request.body);
-    next();
+    await next();
 }
 
-const defaultHandler = (ctx, next, event) => {
-    console.log('Ignoring event', ctx.request.header, event, JSON.stringify(ctx.request.body));
-    next();
+const defaultHandler = async (ctx, next, event) => {
+    console.log('Ignoring event', event, ctx.request.header, JSON.stringify(ctx.request.body));
+    await next();
 };
 const eventHandlers = {
     issues: issuesHandler
 };
 
-module.exports = (ctx, next) => {
+module.exports = async (ctx, next) => {
     const event = ctx.request.header[HEADERS.event];
     const handler = eventHandlers[event] || defaultHandler;
-    handler(ctx, next, event);
-    ctx.status = ctx.status || 202;
+    ctx.status = 202;
+    await handler(ctx, next, event);
+
 }
