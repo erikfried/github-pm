5db40168e7e9eedd44a11fffa19a7cf0aeda5296
Erik Fried <erik.fried@aftonbladet.se>
2017-03-06 17:02:03 +0100

    css instead of js

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,16 +1,1 @@
-if (location.href.indexOf('projects') > 0) {
-    var hideOpenedBy = function(event) {
-        const els = document.querySelectorAll('.project-card small');
-        console.log('hiding elements', els);
-        els.forEach(el => {
-            el.classList.remove('d-block');
-            el.style.display = 'none';
-        });
-    };
-    const to = window.setTimeout(hideOpenedBy, 500);
-    document.addEventListener("load", () => {
-        window.clearTimeout(to);
-        hideOpenedBy
-    });
 
-}
